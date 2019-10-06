f78a91655c8c57546dfc48e93ee388b171f0ae73
TuanLamUet <tupikano1@gmail.com>
2019-10-06 23:58:28 +0700

    Register user part1

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation = {
+    email_incorrect: "Email phải có dạng example@gmail.com",
+    gender_incorrect: "Bạn không thể sửa giới tính",
+    password_incorrect: "Mật khẩu phải chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, chữ số và ký tự đặc biệt ",
+    password_confirmation_incorrect: "Nhập lại mật khẩu chưa chính xác",
+};
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,24 @@
+import {validationResult} from "express-validator/check";
+
 let getLoginRegister = (req, res) => {
     return res.render("./auth/master");
 }
 
+let postRegister = (req, res) => {
+    let errorsArr = [];
+
+    let validationErrors = validationResult(req);
+    if(!validationErrors.isEmpty()) {
+        let errors = Object.values(validationErrors.mapped());
+        errors.forEach(item => {
+            errorsArr.push(item.msg);
+            return;
+        });
+        console.log(errorsArr);
+    }
+    console.log(req.body);
+}
 module.exports = {
-    getLoginRegister: getLoginRegister
+    getLoginRegister: getLoginRegister,
+    postRegister: postRegister
 }
